28bb5146930842542664bb79c2afd6f9e165344e
Vladimir Ryabchenko <79854322262@yandex.ru>
2019-10-20 16:19:44 +0300

    bracketsBalance

---
+++ scripts/bracketsBalance.js
@@ -0,0 +1,40 @@
+const brackets = {
+    '(': ')',
+    '{': '}',
+    '[': ']'
+};
+
+function bracketsBalance(str) {
+    let isBalanced = true;
+    let bracketsString = '';
+
+    for (let i = 0; i < str.length; i++) {
+        if (   str[i] === '(' 
+            || str[i] === '{' 
+            || str[i] === '['
+            || str[i] === ')' 
+            || str[i] === '}' 
+            || str[i] === ']') {
+                bracketsString += str[i];
+        }
+    }
+
+    if (bracketsString.length % 2 === 1) {
+        isBalanced = false;
+        return isBalanced;
+    }
+
+    for (let i = 0; i < (bracketsString.length / 2); i++) {
+        let closingBr = brackets[bracketsString[i]];
+        let start = bracketsString.indexOf(bracketsString[i]);
+        let end = bracketsString.indexOf(closingBr);
+        let substr = bracketsString.slice(start, end + 1);
+
+        if (substr.length % 2 === 1) {
+            isBalanced = false;
+            break;
+        }
+    }
+    
+    return isBalanced;
+}
